d3130010f93f883b92edaf4251864f65c539280e
Luis Molina <luisjose0317@gmail.com>
2025-10-14 09:43:03 -0500

    s

--- globalIsc/backend/api/urls.py
+++ globalIsc/backend/api/urls.py
@@ -7,7 +7,7 @@ from apps.users.api.views.user.index import (
     ForgotPasswordView, PasswordResetConfirmView, CurrentUserView)
 from apps.misc.api.views.companies.index import EmpresaViewSet
 from apps.misc.api.views.roles.index import RolViewSet
-from apps.activesTree.api.views.index import MaquinaViewSet, SyncCompanyRootFolders,FolderViewSet, AnalisisLubricanteViewSet, ResultadoMuestrasAceiteViewSet
+from apps.activesTree.api.views.index import MaquinaViewSet, FolderViewSet, AnalisisLubricanteViewSet, ResultadoMuestrasAceiteViewSet,SyncCompanyRootFolders
 from apps.activesTree.api.views.activesTree.index import ActivesTreeViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from apps.misc.api.views.lubricante.index import LubricanteViewSet
@@ -107,8 +107,9 @@ urlpatterns = [
          ActivesTreeViewSet.as_view({'get': 'flat_structure'}), 
          name='flat-structure'),
 
-    
      path('sync-company-roots/', SyncCompanyRootFolders.as_view(), name='sync-company-roots'),
+     
+     
     path('lubrication/equipment-types/',TipoEquipoViewSet.as_view({
         'get': 'list',
         'post': 'create'
